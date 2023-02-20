f1f87c17cad62f5ea85d15ccdc8c4c3653fee3c3
wahhs <38548736+wahhs@users.noreply.github.com>
2023-02-20 21:26:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,5 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
+echo 'src-git openclash https://github.com/vernesong/OpenClash/tree/master/luci-app-openclash' >>feeds.conf.default
