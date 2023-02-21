e2b6e2072afc83e5c592d46ca498b244a41737be
wahhs <38548736+wahhs@users.noreply.github.com>
2023-02-22 00:03:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,7 +20,7 @@ echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.
 echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 echo 'src-git sulinggg https://github.com/SuLingGG/OpenWrt-Rpi' >>feeds.conf.default
 echo 'src-git lede https://github.com/coolsnowwolf/lede/tree/master' >>feeds.conf.default
-#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 #echo 'src-git appadguardhome https://github.com/Lienol/openwrt/tree/19.07/package/diy/luci-app-adguardhome' >>feeds.conf.default
 #echo 'src-git adguardhome https://github.com/Lienol/openwrt/tree/19.07/package/diy/adguardhome' >>feeds.conf.default
@@ -28,6 +28,6 @@ echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.defa
 
 # theme feed source
 echo 'src-git themedarkmatter https://github.com/apollo-ng/luci-theme-darkmatter' >>feeds.conf.default
-echo 'src-git themeinfinityfreedom src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
+#echo 'src-git themeinfinityfreedom src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
 #echo 'src-git argon https://github.com/jerrykuku/luci-theme-argon' >>feeds.conf.default
 #echo 'src-git argonconfig https://github.com/jerrykuku/luci-app-argon-config' >>feeds.conf.default
