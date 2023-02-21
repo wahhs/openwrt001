0072434fe81a39d92a42e37418ae7706fc634dc6
wahhs <38548736+wahhs@users.noreply.github.com>
2023-02-21 23:48:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -28,6 +28,6 @@ echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.defa
 
 # theme feed source
 echo 'src-git themedarkmatter https://github.com/apollo-ng/luci-theme-darkmatter' >>feeds.conf.default
-echo 'src-git themeinfinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom' >>feeds.conf.default
+echo 'src-git themeinfinityfreedom src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
 #echo 'src-git argon https://github.com/jerrykuku/luci-theme-argon' >>feeds.conf.default
 #echo 'src-git argonconfig https://github.com/jerrykuku/luci-app-argon-config' >>feeds.conf.default
