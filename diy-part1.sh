9ab72d508722dd40546ea6685aea1b5da686b30d
wahhs <38548736+wahhs@users.noreply.github.com>
2023-02-25 23:30:38 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,11 +20,11 @@ echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.
 echo 'src-git openclash https://github.com/vernesong/OpenClash' >>feeds.conf.default
 echo 'src-git sulinggg https://github.com/SuLingGG/OpenWrt-Rpi' >>feeds.conf.default
 #echo 'src-git lede https://github.com/coolsnowwolf/lede/tree/master' >>feeds.conf.default
-echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
-echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+#echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+#echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
 #echo 'src-git appadguardhome https://github.com/Lienol/openwrt/tree/19.07/package/diy/luci-app-adguardhome' >>feeds.conf.default
 #echo 'src-git adguardhome https://github.com/Lienol/openwrt/tree/19.07/package/diy/adguardhome' >>feeds.conf.default
-echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
+#echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 
 # theme feed source
 #echo 'src-git themedarkmatter https://github.com/apollo-ng/luci-theme-darkmatter' >>feeds.conf.default
